7e4af03bd72f4c87c61d1b4adae01e1138e794e4
RealPummel <lukas.nowicki8@gmail.com>
2026-06-10 22:17:59 +0200

    Add different metrics to query

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,9 @@ app = FastAPI(lifespan=lifespan)
 async def get_dm_dashboard(
     project_id: int,
     request: Request,
-    metrics: Optional[List[str]] = Query(None)
+    metrics: Optional[List[str]] = Query(None),
+    criterion_id: Optional[List[str]] = Query(None),
+    alternative_id: Optional[List[str]] = Query(None)
 ):
     supabase: Client = request.app.state.supabase
     
@@ -31,7 +33,20 @@ async def get_dm_dashboard(
     
     if not metrics or "weight" in metrics:
         dashboard.update({"weights": supabase.rpc("get_weight_values_by_project", {"p_id": project_id}).execute().data})
+        
+    if not metrics or "weight_avg" in metrics:
+        if criterion_id:
+            dashboard.update({"weight_avg": supabase.rpc("get_weight_avg_by_criterion", {"p_id": project_id, "c_id": criterion_id}).execute().data})
+        else:
+            dashboard.update({"weight_avg": supabase.rpc("get_weight_avg_by_project", {"p_id": project_id}).execute().data})
+        
+    if not metrics or "alternative_avg" in metrics:
+        if alternative_id:
+            dashboard.update({"alternative_score_avg": supabase.rpc("get_user_score_avg_by_alternative", {"p_id": project_id, "a_id": alternative_id}).execute().data})
+        else:
+            dashboard.update({"alternative_score_avg": supabase.rpc("get_user_score_avg_by_project", {"p_id": project_id}).execute().data})
+        
     if not metrics or "user_score" in metrics:
         dashboard.update({"user_scores": supabase.rpc("get_user_rating_by_project", {"p_id": project_id}).execute().data})
-    
+        
     return dashboard
